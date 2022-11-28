dadb64e288041a44ca79cf4eb59bd2a375a985c7
Teemu Eskola <assariah@gmail.com>
2022-11-28 12:43:48 +0200

    kk

--- JS/indexjs.js
+++ JS/indexjs.js
@@ -1,5 +1,11 @@
 //Here we have the navbar JS
+let link1 = document.getElementById('link2');
 let link2 = document.getElementById('link2');
+let link3 = document.getElementById('link2');
+let link4 = document.getElementById('link2');
+let link5 = document.getElementById('link2');
+let link6 = document.getElementById('link2');
+let link7 = document.getElementById('link2');
 let otter = document.getElementById('otter');
 let home = document.getElementById('home');
 let kiwi = document.getElementById('kiwi');
@@ -29,6 +35,18 @@ function no_animation() {
 }
 
 
+link1.addEventListener('mouseenter', animation);
+link1.addEventListener('mouseleave', no_animation);
 link2.addEventListener('mouseenter', animation);
 link2.addEventListener('mouseleave', no_animation);
+link3.addEventListener('mouseenter', animation);
+link3.addEventListener('mouseleave', no_animation);
+link4.addEventListener('mouseenter', animation);
+link4.addEventListener('mouseleave', no_animation);
+link5.addEventListener('mouseenter', animation);
+link5.addEventListener('mouseleave', no_animation);
+link6.addEventListener('mouseenter', animation);
+link6.addEventListener('mouseleave', no_animation);
+link7.addEventListener('mouseenter', animation);
+link7.addEventListener('mouseleave', no_animation);
 //Navbar JS ends here
