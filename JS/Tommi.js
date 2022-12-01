50d5f1594ef2c72f76e56568b472096fb60024d1
n2jato00 <n2jato00@students.oamk.fi>
2022-12-01 16:33:10 +0200

    scoreboard update

--- JS/Tommi.js
+++ JS/Tommi.js
@@ -1,4 +1,5 @@
-let scoreboard = 0
+let scoreboard = 0;
+
 let button1 = document.getElementById("kysymys1");
 let button2 = document.getElementById("kysymys2");
 let button3 = document.getElementById("kysymys3");
@@ -15,10 +16,11 @@ function firstQuestion(){
     let valinta = document.getElementById("vastaus3");
     if(valinta.checked == true){
         scoreboard = scoreboard + 1
-        document.getElementById("tulos1").innerHTML = "Vastauksesi on oikein" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+        document.getElementById("score").innerHTML = scoreboard
+        document.getElementById("tulos1").innerHTML = "Vastauksesi on oikein"
        
     }else {
-       document.getElementById("tulos1").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Helsinki" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+       document.getElementById("tulos1").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Helsinki"
     }     
 };
 
@@ -26,40 +28,45 @@ function secondQuestion(){
     let valinta = document.getElementById("vastaus5");
     if(valinta.checked == true){
         scoreboard = scoreboard + 1
-        document.getElementById("tulos2").innerHTML = "Vastauksesi on oikein" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+        document.getElementById("score").innerHTML = scoreboard
+        document.getElementById("tulos2").innerHTML = "Vastauksesi on oikein"
        
     }else {
-       document.getElementById("tulos2").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Tanska" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+       document.getElementById("tulos2").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Tanska"
     }     
 };
 function thirdQuestion(){
     let valinta = document.getElementById("vastaus10");
     if(valinta.checked == true){
         scoreboard = scoreboard + 1
-        document.getElementById("tulos3").innerHTML = "Vastauksesi on oikein" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+        document.getElementById("score").innerHTML = scoreboard
+        document.getElementById("tulos3").innerHTML = "Vastauksesi on oikein"
        
     }else {
-       document.getElementById("tulos3").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Pariisi" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+       document.getElementById("tulos3").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Pariisi"
     }     
 };
 function fourthQuestion(){
     let valinta = document.getElementById("vastaus15");
     if(valinta.checked == true){
         scoreboard = scoreboard + 1
-        document.getElementById("tulos4").innerHTML = "Vastauksesi on oikein" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+        document.getElementById("score").innerHTML = scoreboard
+        document.getElementById("tulos4").innerHTML = "Vastauksesi on oikein"
        
     }else {
-       document.getElementById("tulos4").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Mount Everest" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+       document.getElementById("tulos4").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Mount Everest"
     }     
 };
 function fifthQuestion(){
     let valinta = document.getElementById("vastaus18");
     if(valinta.checked == true){
-        scoreboard = scoreboard + 1
-        document.getElementById("tulos5").innerHTML = "Vastauksesi on oikein" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+        scoreboard = scoreboard + 1;
+        document.getElementById("score").innerHTML = scoreboard
+        document.getElementById("tulos5").innerHTML = "Vastauksesi on oikein";
        
     }else {
-       document.getElementById("tulos5").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Ruotsi" + "<br>" + "Sinulla on pisteitä kasassa: " + scoreboard;
+       document.getElementById("tulos5").innerHTML = "Nyt meni väärin, oikea vastaus olisi ollut Ruotsi"
     }     
 };
 
+document.getElementById("score").innerHTML = scoreboard
