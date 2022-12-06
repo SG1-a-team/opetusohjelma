490c48c8c4206e62ea5d4b7a51c3e89174be6293
Sorakuoppa <n2hyti00@students.oamk.fi>
2022-12-06 12:02:16 +0200

    questions added and more js

--- JS/script_timo_hyttinen.js
+++ JS/script_timo_hyttinen.js
@@ -1,40 +1,6 @@
-let question1 = {
-	question: "Mikä on viides planeetta Auringosta?",
-	answerA: "Mars",
-	answerB: "Neptunus",
-	answerC: "Jupiter",
-	answerD: "Merkurius",
-	rightAnswer: answerC
-}
-let question2 = {
-	question: "Ovatko sienet yleensä...",
-	answerA: "Kuluttajia",
-	answerB: "Hajottajia",
-	answerC: "Tuottajia",
-	answerD: "Rakentajia",
-	rightAnswer: answerB
-}
-let question3 = {
-	question: "Mikä näistä linnuista on Suomen kansallislintu?",
-	answerA: "Korppi",
-	answerB: "Laulujoutsen",
-	answerC: "Maakotka",
-	answerD: "Sinirinta",
-	rightAnswer: answerB
-}
-let question4 = {
-	question: "Miksi vuorovesi-ilmiö tapahtuu?",
-	answerA: "Mannerlaattojen liike aiheuttaa merten liikkeen.",
-	answerB: "Maan pyörimisliike aiheuttaa vuorovesi-ilmiön.",
-	answerC: "Merivirtojen liike on niin suurta, että se aiheuttaa ilmiön.",
-	answerD: "Kuun ja Auringon painovoima ja Maan pyörimisliike aiheuttavat sen.",
-	rightAnswer: answerD
-}
-let question5 = {
-	question: "Kuinka monta luuta ihmiskehossa on?",
-	answerA: "206",
-	answerB: "314",
-	answerC: "245",
-	answerD: "180",
-	rightAnswer: answerA
-}
+let startButton = document.getElementById("startButton");
+let nextButton = document.getElementById("nextButton")
+startButton.addEventListener("click", function(){
+	document.getElementById("divQuestion1").classList.remove("disappear");
+	} 
+)
