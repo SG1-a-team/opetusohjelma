a0e9cfdfc7ced2fdc7af1b8598a4b444f69d5ccd
Sorakuoppa <n2hyti00@students.oamk.fi>
2022-12-12 14:23:02 +0200

    reward message added

--- JS/script_timo_hyttinen.js
+++ JS/script_timo_hyttinen.js
@@ -1,3 +1,4 @@
+/* Stable variables for some elements from HTML */
 let startButton = document.getElementById("startButton");
 let nextButton = document.getElementById("nextButton");
 let questionDiv = document.getElementById("question");
@@ -6,7 +7,8 @@ let answerButton2 = document.getElementById("answer2");
 let answerButton3 = document.getElementById("answer3");
 let answerButton4 = document.getElementById("answer4");
 let starOverButton = document.getElementById("soButton");
-
+let reward = document.getElementById("rewards");
+/* Array for questions. */
 let questions = [
 	{
 	question: "Mikä on viides planeetta Auringosta?",
@@ -37,18 +39,18 @@ let questions = [
 	answerD: "Merivirtojen liike on niin suurta, että se aiheuttaa ilmiön",
 	},
 	{
-	question: "Kuinka monta luuta ihmiskehossa on?",
+	question: "Kuinka monta luuta on aikuisessa ihmisessä?",
 	answerA: "314",
 	answerB: "245",
 	answerC: "180",
 	answerD: "206",
 	}
 ]
-
+/* Counter variables for needed counters. */
 let counter = 0;
 let rightAnswerCounter = 0;
 let currentQuestion = 0;
-
+/* Function and eventlistener for start button. */
 startButton.addEventListener("click", function(){
 	currentQuestion = 0;
 	document.getElementById("divQuestion").classList.remove("disappear");
@@ -66,7 +68,7 @@ startButton.addEventListener("click", function(){
 	answerButton4.textContent = questions[currentQuestion].answerD;
 	}
 )
-
+/* Next four function-eventlistener combos for each answer buttons. And yes, these are long and there is lots of repetition. */
 answerButton1.addEventListener("click", function() {
 	if (currentQuestion === 3){
 		counter = counter + 1;
@@ -78,14 +80,51 @@ answerButton1.addEventListener("click", function() {
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
-	} else {
+	} if (currentQuestion === 0) {
 		counter = counter + 1;
 		document.getElementById("counter").innerHTML = counter;
-		document.getElementById("answersResult").innerHTML = " väärin.";
+		document.getElementById("answersResult").innerHTML = " väärin. Oikea vastaus on Jupiter.";
 		answerButton1.disabled = true;
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
+	}if (currentQuestion === 1) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Sienet ovat yleensä hajottajia.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}if (currentQuestion === 2) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Laulujoutsen on Suomen kansallislintu.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}if (currentQuestion === 4) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Aikuisessa ihmisessä on 206 luuta.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}
+	if (rightAnswerCounter === 0) {
+		reward.innerHTML = ZERO;
+	} if (rightAnswerCounter === 1) {
+		reward.innerHTML = ONE;
+	} if (rightAnswerCounter === 2) {
+		reward.innerHTML = TWO;
+	} if (rightAnswerCounter === 3) {
+		reward.innerHTML = THREE;
+	} if (rightAnswerCounter === 4) {
+		reward.innerHTML = FOUR;
+	} if (rightAnswerCounter === 5) {
+		reward.innerHTML = FIVE;
 	}
 	}
 )
@@ -100,15 +139,43 @@ answerButton2.addEventListener("click", function() {
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
-	}
-	else {
+	}if (currentQuestion === 0) {
 		counter = counter + 1;
 		document.getElementById("counter").innerHTML = counter;
-		document.getElementById("answersResult").innerHTML = " väärin.";
+		document.getElementById("answersResult").innerHTML = " väärin. Oikea vastaus on Jupiter.";
 		answerButton1.disabled = true;
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
+	}if (currentQuestion === 3) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Sekä kuun ja auringon painovoima että Maan pyörimisliike vaikuttavat siihen.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}if (currentQuestion === 4) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Aikuisessa ihmisessä on 206 luuta.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}
+	if (rightAnswerCounter === 0) {
+		reward.innerHTML = ZERO;
+	} if (rightAnswerCounter === 1) {
+		reward.innerHTML = ONE;
+	} if (rightAnswerCounter === 2) {
+		reward.innerHTML = TWO;
+	} if (rightAnswerCounter === 3) {
+		reward.innerHTML = THREE;
+	} if (rightAnswerCounter === 4) {
+		reward.innerHTML = FOUR;
+	} if (rightAnswerCounter === 5) {
+		reward.innerHTML = FIVE;
 	}
 	}
 )
@@ -123,14 +190,51 @@ answerButton3.addEventListener("click", function() {
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
-	} else {
+	} if (currentQuestion === 1) {
 		counter = counter + 1;
 		document.getElementById("counter").innerHTML = counter;
-		document.getElementById("answersResult").innerHTML = " väärin.";
+		document.getElementById("answersResult").innerHTML = " väärin. Sienet ovat yleensä hajottajia.";
 		answerButton1.disabled = true;
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
+	}if (currentQuestion === 2) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Laulujoutsen on Suomen kansallislintu.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}if (currentQuestion === 3) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Sekä kuun ja auringon painovoima että Maan pyörimisliike vaikuttavat siihen.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}if (currentQuestion === 4) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Aikuisessa ihmisessä on 206 luuta.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	}
+	if (rightAnswerCounter === 0) {
+		reward.innerHTML = ZERO;
+	} if (rightAnswerCounter === 1) {
+		reward.innerHTML = ONE;
+	} if (rightAnswerCounter === 2) {
+		reward.innerHTML = TWO;
+	} if (rightAnswerCounter === 3) {
+		reward.innerHTML = THREE;
+	} if (rightAnswerCounter === 4) {
+		reward.innerHTML = FOUR;
+	} if (rightAnswerCounter === 5) {
+		reward.innerHTML = FIVE;
 	}
 	}
 )
@@ -145,19 +249,62 @@ answerButton4.addEventListener("click", function() {
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
-	}
-	else {
+	} if (currentQuestion === 0) {
 		counter = counter + 1;
 		document.getElementById("counter").innerHTML = counter;
-		document.getElementById("answersResult").innerHTML = " väärin.";
+		document.getElementById("answersResult").innerHTML = " väärin. Oikea vastaus on Jupiter.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	} if (currentQuestion === 1) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Sienet ovat yleensä hajottajia.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	} if (currentQuestion === 2) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Laulujoutsen on Suomen kansallislintu.";
+		answerButton1.disabled = true;
+		answerButton2.disabled = true;
+		answerButton3.disabled = true;
+		answerButton4.disabled = true;
+	} if (currentQuestion === 3) {
+		counter = counter + 1;
+		document.getElementById("counter").innerHTML = counter;
+		document.getElementById("answersResult").innerHTML = " väärin. Sekä kuun ja auringon painovoima että Maan pyörimisliike vaikuttavat siihen.";
 		answerButton1.disabled = true;
 		answerButton2.disabled = true;
 		answerButton3.disabled = true;
 		answerButton4.disabled = true;
 	}
+	if (rightAnswerCounter === 0) {
+		reward.innerHTML = ZERO;
+	} if (rightAnswerCounter === 1) {
+		reward.innerHTML = ONE;
+	} if (rightAnswerCounter === 2) {
+		reward.innerHTML = TWO;
+	} if (rightAnswerCounter === 3) {
+		reward.innerHTML = THREE;
+	} if (rightAnswerCounter === 4) {
+		reward.innerHTML = FOUR;
+	} if (rightAnswerCounter === 5) {
+		reward.innerHTML = FIVE;
+	}
 	}
 )
+const ZERO = "Oot aika mato!";
+const ONE = "Vielä pitää harjoitella!";
+const TWO = "Olet vielä ravintoketjun pohjalla.";
+const THREE = "Ihan jeesh!";
+const FOUR = "Olet yhtä fiksu kuin simpanssi!";
+const FIVE = "Ympäristötiedon kunkku!";
 
+/* Function with eventlistener for next button. */
 nextButton.addEventListener("click", function(){
 	currentQuestion = currentQuestion + 1;
 	console.log(currentQuestion);
@@ -171,10 +318,9 @@ nextButton.addEventListener("click", function(){
 	answerButton2.disabled = false;
 	answerButton3.disabled = false;
 	answerButton4.disabled = false;
-})
-
-/* function nextQuestion()  */
-
+	}
+)
+/* Function with eventlistener for star over button. */
 soButton.addEventListener("click", function(){
 	currentQuestion = 0;
 	document.getElementById("divQuestion").classList.add("disappear");
@@ -184,6 +330,7 @@ soButton.addEventListener("click", function(){
 	document.getElementById("answerP").classList.add("disappear");
 	document.getElementById("pointDiv").classList.add("disappear");
 	document.getElementById("answersResult").innerHTML = "";
+	reward.innerHTML = "";
 	counter = 0;
 	rightAnswerCounter = 0;
 	document.getElementById("counter").innerHTML = "";
@@ -192,7 +339,6 @@ soButton.addEventListener("click", function(){
 	answerButton2.disabled = false;
 	answerButton3.disabled = false;
 	answerButton4.disabled = false;
-})
-
-/* document.getElementById("counter").innerHTML = counter;
-document.getElementById("correct").innerHTML = rightAnswerCounter; */
+	nextButton.disabled = false;
+	}
+)
